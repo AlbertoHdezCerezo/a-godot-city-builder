9b3c3a67aa279b0dad278a7d5769109787458ffd
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-03-15 10:02:22 +0100

    feat: Building resource
    Defines a new `Resource` that models a building. A building has a packed scene containing the building 3D model, plus some additional information, such as the name of the building and its price.

---
+++ resources/building.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name Building
+
+@export_subgroup("Design")
+@export var model: PackedScene
+
+@export_subgroup("Metadata")
+@export var name: String
+@export var price: int
